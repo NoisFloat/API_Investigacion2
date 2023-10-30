3f6fa848940fa02124eed53590f017fe82b61e03
NoisFloat <144391743+NoisFloat@users.noreply.github.com>
2023-10-29 23:42:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,139 +4,100 @@ const bdCarreras = require('./datos/bdCarreras.json');
 const bdInscripciones = require('./datos/bdInscripciones.json');
 const fs = require('fs');
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).send("Hola Mundo");
-  res.send()
-
 });
 
 app.get('/API/carreras', (req, res) => {
-  res.status(200).send(bdCarreras);
+  res.status(200).json(bdCarreras);
 });
 
 app.get('/API/inscripciones', (req, res) => {
-  res.status(200).send(bdInscripciones);
+  res.status(200).json(bdInscripciones);
 });
 
-//● Deberá haber una ruta para consulta de prerrequisitos de una materia por código.
-
-//Para el tecnico
 app.get('/API/carreras/tecnico/materia/prerrequisitos/:codigoM', (req, res) => {
-  const codigoM = req.params.codigoM; 
-  console.log(codigoM);
-
+  const codigoM = req.params.codigoM;
   const tecnico = bdCarreras.carreras.find(carrera => carrera.nombre === "Técnico en Ingeniería en Computación");
   const materia = tecnico.materias.find(materia => materia.codigo === codigoM);
-  // Devuelve los prerrequisitos de la materia en formato JSON
-  res.json(materia.prerrequisitos);
+  if (materia) {
+    res.json(materia.prerrequisitos);
+  } else {
+    res.status(404).json({ error: 'Materia no encontrada' });
+  }
 });
 
-
-//Para el tecnico
 app.get('/API/carreras/ingenieria/materia/prerrequisitos/:codigoM', (req, res) => {
-  const codigoM = req.params.codigoM; 
-  console.log(codigoM);
-
-  const tecnico = bdCarreras.carreras.find(carrera => carrera.nombre === "Ingeniería en Ciencias de la Computación");
-  const materia = tecnico.materias.find(materia => materia.codigo === codigoM);
-  // Devuelve los prerrequisitos de la materia en formato JSON
-  res.json(materia.prerrequisitos);
+  const codigoM = req.params.codigoM;
+  const ingenieria = bdCarreras.carreras.find(carrera => carrera.nombre === "Ingeniería en Ciencias de la Computación");
+  const materia = ingenieria.materias.find(materia => materia.codigo === codigoM);
+  if (materia) {
+    res.json(materia.prerrequisitos);
+  } else {
+    res.status(404).json({ error: 'Materia no encontrada' });
+  }
 });
-// FIN Deberá haber una ruta para consulta de prerrequisitos de una materia por código.
 
-
-
-
-
-//● Deberá crear un ruta para consulta de materias por ciclo.
-
-//Busca materias del ciclo espeficicado siendo del tecnico
 app.get('/API/carreras/tecnico/ciclo/:nCiclo', (req, res) => {
-  const nCiclo = parseInt(req.params.nCiclo); 
-
+  const nCiclo = parseInt(req.params.nCiclo);
   const tecnico = bdCarreras.carreras.find(carrera => carrera.nombre === "Técnico en Ingeniería en Computación");
-
   const materiasCiclo = tecnico.materias.filter(materia => materia.ciclo === nCiclo);
-
-  // Envía las materias como respuesta en formato JSON
   res.json(materiasCiclo);
 });
 
-//Busca materias del ciclo espeficicado siendo de la ingenieria
 app.get('/API/carreras/ingenieria/ciclo/:nCiclo', (req, res) => {
-  const nCiclo = parseInt(req.params.nCiclo); 
-  const tecnico = bdCarreras.carreras.find(carrera => carrera.nombre === "Ingeniería en Ciencias de la Computación");
-  const materiasCiclo = tecnico.materias.filter(materia => materia.ciclo === nCiclo);
-
-  // Envía las materias como respuesta en formato JSON
+  const nCiclo = parseInt(req.params.nCiclo);
+  const ingenieria = bdCarreras.carreras.find(carrera => carrera.nombre === "Ingeniería en Ciencias de la Computación");
+  const materiasCiclo = ingenieria.materias.filter(materia => materia.ciclo === nCiclo);
   res.json(materiasCiclo);
 });
-//●FIN Deberá crear un ruta para consulta de materias por ciclo.
 
-
-
-
-
-
-//Inscripcion de materias
-// Ruta para inscribir materias
-app.post('/inscripciones/:carrera', (req, res) => {
-  const carrera = req.params.carrera;
-  const { materias } = req.body;
-
-  if (!carrera || !materias) {
-    res.status(400).json({ error: 'Faltan datos necesarios.' });
-    return;
-  }
-
-  if (materias.length !== 4) {
-    res.status(400).json({ error: 'Debes inscribir exactamente 4 materias.' });
-    return;
-  }
-
-  const materiasDisponibles = obtenerMaterias();
-
-  if (!materiasDisponibles[carrera]) {
-    res.status(404).json({ error: 'La carrera seleccionada no existe.' });
-    return;
-  }
-
-  const materiasNoEncontradas = materias.filter((materia) => !materiasDisponibles[carrera].includes(materia));
-
-  if (materiasNoEncontradas.length > 0) {
-    res.status(400).json({ error: `Las siguientes materias no existen: ${materiasNoEncontradas.join(', ')}` });
-    return;
-  }
-
-  const inscripciones = obtenerInscripciones();
-  inscripciones[carrera] = materias;
-  guardarInscripciones(inscripciones);
-
-  res.status(200).json({ message: 'Inscripción exitosa.' });
-});
-
-app.listen(PORT, () => {
-  console.log(`Servidor Express en funcionamiento en el puerto ${PORT}`);
-});
-
-function obtenerMaterias() {
-  const data = fs.readFileSync('materias.json', 'utf8');
-  return JSON.parse(data);
-}
-
-function obtenerInscripciones() {
-  try {
-    const data = fs.readFileSync('./datos/bdInscripciones.json', 'utf8');
-    return JSON.parse(data);
-  } catch (error) {
-    return {};
-  }
-}
-
-function guardarInscripciones(inscripciones) {
-  fs.writeFileSync('inscripciones.json', JSON.stringify(inscripciones, null, 2));
-}
+//
+app.post('/inscribir', (req, res) => {
+    try {
+      // Verificar si el archivo JSON de inscripciones existe
+      const filePath = './datos/bdInscripciones.json';
+      let inscripciones = [];
+  
+      if (fs.existsSync(filePath)) {
+        // Leer el contenido del archivo si existe
+        const fileContent = fs.readFileSync(filePath, 'utf-8');
+        // Intenta parsear el contenido como JSON
+        inscripciones = JSON.parse(fileContent);
+  
+        // Verifica si el resultado es un array, y si no, inicializa un nuevo array vacío
+        if (!Array.isArray(inscripciones)) {
+          inscripciones = [];
+        }
+      }
+  
+      // Obtener los datos de la inscripción del cuerpo de la solicitud
+      const inscripcion = req.body;
+       console.log(inscripcion)
+      // Asigna un ID único a la inscripción
+      inscripcion.id = inscripciones.length + 1;
+  
+      // Agregar la inscripción al arreglo de inscripciones
+      inscripciones.push(inscripcion);
+  
+      // Escribir el archivo JSON de inscripciones
+      fs.writeFileSync(filePath, JSON.stringify(inscripciones, null, 2));
+      res.status(201).json({ message: 'Inscripción exitosa', id: inscripcion.id });
+    } catch (error) {
+      console.error('Error al escribir el archivo de inscripciones:', error);
+      res.status(500).json({ error: 'Error al guardar la inscripción' });
+    }
+  });
+  
+  
+  
+  
+  
+  
+  
+//
 
 const puerto = 3000;
 app.listen(puerto, () => {
